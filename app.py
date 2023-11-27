80f55da42146d68c42a98aaf2507f48bd4c6e918
Saptak Sadhya <sadhyasaptak1998@gmail.com>
2023-11-28 00:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.header("Enter the details of your house to get price")
 sqft = st.slider('Total Square Feet', min_value=100, max_value=10000, value=1000, step=100)
 bath = st.number_input('Number of Bathrooms', min_value=1, max_value=10, value=2)
 bhk = st.number_input('Number of Bedrooms (BHK)', min_value=1, max_value=10, value=2)
-balcony = st.number_input('Number of Bathrooms', min_value=1, max_value=5, value=2)
+balcony = st.number_input('Number of Balcony', min_value=1, max_value=5, value=2)
 
 def predict_price(sqft,bath,bhk,balcony):
     inp = np.array([sqft,bath,bhk,balcony]).reshape(1,-1)
